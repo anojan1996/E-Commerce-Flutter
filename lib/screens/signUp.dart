34edbb49c97b09148cb0529293626966c1560aec
anojan1996 <anojan2012@gmail.com>
2022-11-21 23:27:44 +0530

    Register Page UI

--- lib/screens/signUp.dart
+++ lib/screens/signUp.dart
@@ -7,9 +7,123 @@ class SignUp extends StatefulWidget {
   State<SignUp> createState() => _SignUpState();
 }
 
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 250,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      const Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  height: 350,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'User Name',
+                          border: const OutlineInputBorder(),
+                          hintStyle: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: const OutlineInputBorder(),
+                          hintStyle: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          suffixIcon: GestureDetector(
+                            onTap: (() {
+                              FocusScope.of(context).unfocus();
+                            }),
+                            child: Icon(
+                              Icons.visibility,
+                              color: Colors.black,
+                            ),
+                          ),
+                          border: const OutlineInputBorder(),
+                          hintStyle: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Phone Number',
+                          suffixIcon: GestureDetector(
+                            onTap: (() {
+                              FocusScope.of(context).unfocus();
+                            }),
+                            child: Icon(
+                              Icons.visibility,
+                              color: Colors.black,
+                            ),
+                          ),
+                          border: const OutlineInputBorder(),
+                          hintStyle: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Register'),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text('I have already an account!'),
+                          // SizedBox(width: 20,),
+                          GestureDetector(
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.cyan,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
