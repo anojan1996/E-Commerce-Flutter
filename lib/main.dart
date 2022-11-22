d80e2892310529afe0146cfae822c8a709b2dff3
anojan1996 <anojan2012@gmail.com>
2022-11-22 23:45:07 +0530

    Firebase Set Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/screens/login.dart';
 import 'package:e_commerce/screens/signUp.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
